e546f1e3ed065e3dc48bf02cbf780b33eaad3518
Haroon Mahmood <148611250+haroonaulakh@users.noreply.github.com>
2026-02-23 13:35:03 +0500

    Update main.py (Add frontend url in main.py)

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://crevo-seven.vercel.app",
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
